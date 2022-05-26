dc038cf5f325e92e1f22d27cc16c2c04f1d9bc45
Felix-Fenix <happyhuman@bk.ru>
2022-05-26 21:06:16 +0700

    lesson_1.

--- library/settings.py
+++ library/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-g14+q$3v@^vjw+^w2kwme75pcu96k4*rjo*3)10&gi72t=r@pe
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'authors',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
